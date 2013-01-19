81bd45a9d21272dda20b65db7819ef9c11188dea
sunny8705 <sunny8705@126.com>
2013-01-19 00:17:00 -0800

    Create OrthoList.cpp

---
+++ OrthoList.cpp
@@ -0,0 +1,13 @@
+// OrthoList.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "OrthoListTest.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+  OrthoListTest::BeginTest();
+	system("pause");
+	return 0;
+}
+
